9ea050f8c33f319de4e55658b189e5b288fcb1e1
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-09-16 10:57:15 +0430

    add apply and applyFliped

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -1,10 +1,8 @@
 module Ch5 where
 
-import Prelude (Unit)
-
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, show)
+import Prelude (Unit, discard, show)
 
 
 appendA :: Boolean -> Int -> String
@@ -28,9 +26,24 @@ const a _ = a
 alwaysTrue :: Int -> Boolean
 alwaysTrue = const true 
 
+apply :: ∀ a b. (a -> b) -> a -> b
+apply f x = f x
+
+infixr 0 apply as $
+
+applyFlipped :: ∀ a b. a -> (a -> b) -> b
+applyFlipped = flip apply
+
+infixl 1 applyFlipped as #
+
 test :: Effect Unit
 test = do
   -- log (show (flip appendA 1 false))
   -- log (show (const 69 "sechs"))
-  log (show ( alwaysTrue 2 ))
-  
+  -- log (show ( ( apply appendA false ) 10 ))
+  -- log (show ( let ff = appendA false in ff 10 ))
+  -- log $ show $ ( apply appendA false ) 10
+  -- log ( show $ ( apply appendA false ) 10 )
+  -- log $ show $ apply appendA false  10
+  log $ show $ flip const 1 2
+  flip const 1 2 # show # log
