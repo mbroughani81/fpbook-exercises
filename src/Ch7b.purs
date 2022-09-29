4349fe3d28ce9ba20c504516ee2b570a6109c14e
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-09-30 02:26:13 +0330

    test FromCSV

--- src/Ch7b.purs
+++ src/Ch7b.purs
@@ -12,49 +12,37 @@ import Effect (Effect)
 import Effect.Console (log)
 
 newtype CSV = CSV String
-class ToCSV a where
-  toCSV :: a -> CSV
-
-newtype FullName = FullName String
-newtype Age = Age Int
-data Occupation = Doctor | Dentist | Lawyer | Unemployed
-data Person = Person
-  { name :: FullName
-  , age :: Age
-  , occupation :: Occupation
-  }
-
--- derive instance newtypeFullName :: Newtype FullName _
--- derive newtype instance showFullName :: Show FullName
-instance showFullName :: Show FullName where
-  show (FullName name) = name
-
-derive instance newtypeAge :: Newtype Age _
-derive newtype instance showAge :: Show Age
-
-derive instance genericOccupation :: Generic Occupation _
-instance showOccupation :: Show Occupation where
-  show = genericShow
-
-instance toCSVPerson :: ToCSV Person where
-  toCSV (Person {name, age, occupation}) =
-    CSV $ show name <> "," <> show age <> "," <> show occupation
+derive instance newtypeCSV :: Newtype CSV _
 derive instance eqCSV :: Eq CSV
 
+class ToCSV a where
+  toCSV :: a -> CSV
 class FromString a where
   fromString :: String -> Maybe a
 
 class FromCSV a where
   fromCSV :: CSV -> Maybe a
 
+newtype FullName = FullName String
+instance showFullName :: Show FullName where
+  show (FullName name) = name
 instance fromStringFullName :: FromString FullName where
   fromString s = Just (FullName s)
-
+  
+newtype Age = Age Int
+derive instance newtypeAge :: Newtype Age _
+derive newtype instance showAge :: Show Age
 instance fromStringAge :: FromString Age where
   fromString s = case FromIntToString.fromString s of
     Just x -> Just (Age x)
     Nothing -> Nothing
 
+
+data Occupation = Doctor | Dentist | Lawyer | Unemployed
+derive instance eqOccupation :: Eq Occupation
+derive instance genericOccupation :: Generic Occupation _
+instance showOccupation :: Show Occupation where
+  show = genericShow
 instance fromStringOccupation :: FromString Occupation where
   fromString s = case s of 
     "Doctor" -> Just Doctor
@@ -63,6 +51,19 @@ instance fromStringOccupation :: FromString Occupation where
     "Unemployed" -> Just Unemployed
     _ -> Nothing
 
+data Person = Person
+  { name :: FullName
+  , age :: Age
+  , occupation :: Occupation
+  }
+instance eqPerson :: Eq Person where
+  eq 
+    (Person {name: FullName f1, age: Age a1, occupation: o1})
+    (Person {name: FullName f2, age: Age a2, occupation: o2}) =  
+    f1 == f2 && a1 == a2 && o1 == o2
+instance toCSVPerson :: ToCSV Person where
+  toCSV (Person {name, age, occupation}) =
+    CSV $ show name <> "," <> show age <> "," <> show occupation
 instance fromCSVPerson :: FromCSV Person where
   fromCSV (CSV s) = case split (Pattern ",") s of
     -- interesting thing: 
@@ -83,3 +84,5 @@ test = do
   log $ show $ let x = (fromCSV :: CSV -> Maybe Person) (CSV "mb,20,Unemployed") in case x of 
     Just xx -> "gg"
     _ -> "Nothing"
+  let person = Person { name: FullName "mb", age: Age 18, occupation: Unemployed }
+  log $ show $ (fromCSV $ toCSV $ person) == Just person
