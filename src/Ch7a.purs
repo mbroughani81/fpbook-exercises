08c73c917066db759549abfbe4866c0c6274cb80
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-09-29 21:18:24 +0330

    derive instance for maybe

--- src/Ch7a.purs
+++ src/Ch7a.purs
@@ -1,34 +1,44 @@
 module Ch7a where
 
 import Data.Eq (class Eq)
+import Data.Generic.Rep (class Generic)
 import Data.Ord (class Ord, Ordering(..), compare)
 import Data.Show (class Show, show)
+import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, discard, (==), ($), (<), (>), (<=), (||), (<>))
+import Prelude (Unit, discard, (==), ($), (<), (>), (<=), (||))
+
 
 data Maybe a = Nothing | Just a
 
-instance eqMaybe :: Eq a => Eq (Maybe a) where
-  eq Nothing Nothing = true
-  eq (Just x) (Just y) = x == y
-  eq _ _ = false
+-- instance eqMaybe :: Eq a => Eq (Maybe a) where
+--   eq Nothing Nothing = true
+--   eq (Just x) (Just y) = x == y
+--   eq _ _ = false
+derive instance eqMaybe :: Eq a => Eq (Maybe a)
 
-instance ordMaybe :: Ord a => Ord (Maybe a) where
-  compare Nothing Nothing = EQ
-  compare Nothing _ = LT
-  compare (Just x) (Just y) = compare x y
-  compare (Just x) _ = GT
-
-instance showMaybe :: Show a => Show (Maybe a) where
-  show Nothing = "Nothing"
-  show (Just x) = "(Just " <> show x <> ")"
+-- instance ordMaybe :: Ord a => Ord (Maybe a) where
+--   compare Nothing Nothing = EQ
+--   compare Nothing _ = LT
+--   compare (Just x) (Just y) = compare x y
+--   compare (Just x) _ = GT
 -- ordMaybe with signature Ord (Maybe a) won't work, because it expects 
 -- to be an Eq instance of Maybe a, but out eqMaybe has sign Eq a => Eq (Maybe a). 
 -- so it dosen't cover Eq of all possible "Maybe a"s
 
 -- ordMaybe with signature Ord a or Eq a works, because we have Eq a => Eq (Maybe a)
--- instance!  
+-- instance!
+derive instance ordMaybe :: Ord a => Ord (Maybe a)
+
+-- instance showMaybe :: Show a => Show (Maybe a) where
+--   show Nothing = "Nothing"
+--   show (Just x) = "(Just " <> show x <> ")"
+
+derive instance genericMaybe :: Generic (Maybe a) _
+instance showMaybe :: Show a => Show (Maybe a) where
+  show = genericShow
+
 
 greaterThanOrEq :: ∀ a. Ord a => a -> a -> Boolean
 greaterThanOrEq x y = cmp == EQ || cmp == GT where cmp = compare x y
