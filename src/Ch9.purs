d8cb94da8289b0b6d1b40a9404759c644b86f4cb
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-10-05 13:51:40 +0330

    AndBool monoid and semigroup

---
+++ src/Ch9.purs
@@ -0,0 +1,55 @@
+module Ch9 where
+
+import Data.Generic.Rep (class Generic)
+import Data.Show (class Show, show)
+import Data.Show.Generic (genericShow)
+import Effect (Effect)
+import Effect.Console (log)
+import Prelude (Unit, class Eq, ($), (==), (&&), discard)
+
+class Semigroup a where
+  append :: a -> a -> a
+infixr 5 append as <>
+
+class Semigroup a <= Monoid a where
+  mempty :: a
+
+data AndBool = AFalse | ATrue
+derive instance eqAndBool :: Eq AndBool
+derive instance genericAndBool :: Generic AndBool _
+
+instance showAndBool :: Show AndBool where
+  show = genericShow
+
+instance semigroupAndBool :: Semigroup AndBool where
+  append ATrue ATrue = ATrue
+  append _ _ = AFalse
+
+instance monoidAndBool :: Monoid AndBool where
+  mempty = ATrue
+
+
+verifyAndBoolSemigroup :: Effect Unit
+verifyAndBoolSemigroup = do
+  log "Verifying AndBool Semigroup Laws (1 test)"
+  log $ show $ (AFalse <> ATrue) <> ATrue == AFalse <> (ATrue <> ATrue)
+
+verifyAndBoolMonoid :: Effect Unit
+verifyAndBoolMonoid = do
+  log "Verifying AndBool Monoid Laws (2 tests)"
+  log $ show $ (ATrue <> mempty) == ATrue && ATrue == (ATrue <> mempty)
+  log $ show $ (AFalse <> mempty) == AFalse && AFalse == (AFalse <> mempty)
+
+test :: Effect Unit
+test = do
+  log $ show $ ATrue <> ATrue
+  log $ show $ ATrue <> AFalse
+  log $ show $ AFalse <> ATrue
+  log $ show $ "----------------------"
+  log $ show $ mempty <> ATrue == ATrue
+  log $ show $ mempty <> AFalse == AFalse
+  log $ show $ "----------------------"
+  verifyAndBoolSemigroup
+  log $ show $ "----------------------"
+  verifyAndBoolMonoid
+
