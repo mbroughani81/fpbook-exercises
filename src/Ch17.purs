66b4f7a09f64c982adac6afdd0247d8380283b18
Mohammad Broughani <gitmbroughani81@gmail.com>
2022-10-30 23:54:58 +0330

    implement applicativeEither

--- src/Ch17.purs
+++ src/Ch17.purs
@@ -2,6 +2,7 @@ module Ch17 where
 
 import Prelude
 
+import Data.Bifunctor (class Bifunctor, bimap)
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
 import Effect (Effect)
@@ -32,6 +33,33 @@ instance applicativeMaybe :: Applicative Maybe where
   pure :: ∀ a. a -> Maybe a
   pure = Just
 
+data Either a b = Left a | Right b
+
+derive instance eqEither :: (Eq a, Eq b) => Eq (Either a b)
+
+derive instance ordEither :: (Ord a, Ord b) => Ord (Either a b)
+
+derive instance functorEither :: Functor (Either a)
+
+derive instance genericEither :: Generic (Either a b) _
+
+instance showEither :: (Show a, Show b) => Show (Either a b) where
+  show = genericShow
+
+instance bifunctorEither :: Bifunctor Either where
+  bimap :: ∀ a b c d. (a -> b) -> (c -> d) -> Either a c -> Either b d
+  bimap f _ (Left x) = Left $ f x
+  bimap _ g (Right y) = Right $ g y
+
+instance applyEither :: Apply (Either a) where
+  apply :: ∀ b c. Either a (b -> c) -> Either a b -> Either a c
+  apply (Left err) _ = Left err
+  apply (Right f) x = f <$> x
+
+instance applicativeEither :: Applicative (Either a) where
+  pure :: ∀ b. b -> Either a b
+  pure = Right 
+
 test :: Effect Unit
 test = do
   log "placeholder"
@@ -46,3 +74,24 @@ test = do
   log $ show $ (+) <$> Just 21 <*> Just 21
   log $ show $ (*) <$> pure 2 <*> (pure 21 :: Maybe Int)
   log $ show $ pure (+) <*> Just 17 <*> Just 25 
+  log $ show $ (Left "error" :: Either String Unit)
+  log $ show $ ((Left "error" :: Either String Unit) == (Left "grror" :: Either String Unit))
+  log $ show $ ((Right 2 :: Either String Int) == (Left "22" :: Either String Int))
+  log $ show $ (_ * 2) <$> (Right 20 :: Either String Int)
+  log $ show $ (_ * 2) <$> (Left "str" :: Either String Int)
+  log $ show $ bimap (_ * 2) (_ * 10) (Left 20 :: Either Int Int)
+  log $ show $ (Right (_ * 2) :: Either Int (Int -> Int)) <*> (Right 20 :: Either Int Int)
+  log $ show $ (Left 2000 :: Either Int (Int -> Int)) <*> (Right 20 :: Either Int Int)
+
+
+  log $ show $ ((<<<) <$> pure identity <*> pure identity <*> pure 1) ==
+    (pure identity <*> (pure identity <*> pure 1) :: Either Unit Int)
+  -- LAW: Identity
+  -- pure identity <*> x = x
+  log $ show $ (pure identity <*> pure 1) == (pure 1 :: Either Unit Int)
+  -- LAW: Homomorphism
+  -- pure (f x) = pure f <*> pure x
+  log $ show $ pure (negate 1) == (pure negate <*> pure 1 :: Either Unit Int)
+  -- LAW: Interchange
+  -- u <*> pure x = pure (_ $ x) <*> u
+  log $ show $ (pure negate <*> pure 1) == (pure (_ $ 1) <*> pure negate :: Either Unit Int)
